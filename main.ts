72ca52952f8a2181da691a8225571e195ae0e1ad
tls205 <80431250+tls205@users.noreply.github.com>
2021-06-14 20:21:56 +1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let hand = 0
+input.onGesture(Gesture.Shake, function () {
+    hand = randint(1, 3)
+    if (hand == 1) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    } else if (hand == 2) {
+    	
+    } else {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    }
+})
